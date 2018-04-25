f6b416be17076a5bb7212641270835aa45c29b7e
projectfox <sam.storbacka@hotmail.com>
2018-04-25 13:08:16 +0300

    added gitignore and base repository

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameControl : MonoBehaviour {
+
+    public static GameControl instance;
+    public GameObject gameOverText;
+    public bool gameOver = false;
+    public float scrollSpeed = -1.5f;
+
+    void Awake () {
+        if (instance == null) {
+            instance = this;
+        }
+        else if (instance != this) {
+            Destroy(gameObject);
+        }
+    }
+	
+	void Update () {
+	    if (gameOver && Input.GetMouseButtonDown(0)) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+	}
+
+    public void BirdDied () {
+        gameOverText.SetActive (true);
+        gameOver = true;
+    }
+}
